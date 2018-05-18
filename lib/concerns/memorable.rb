9d930eed76b5acc42bf53e0129e70d1f00479e3c
Anthony Lam <al3101@nyu.edu>
2018-05-18 16:17:05 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
-  module Clas
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
   
 end
