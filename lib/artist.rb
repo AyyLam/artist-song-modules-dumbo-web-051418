487f2b4393ac73097de3954ef081e547c15be959
Anthony Lam <al3101@nyu.edu>
2018-05-18 16:11:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,9 +10,6 @@ class Artist
 
   @@artists = []
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 
   def initialize
     @@artists << self
@@ -23,14 +20,6 @@ class Artist
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   @@artists.count
-  # end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -40,8 +29,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 
 end
